e5eb3d5f76e5bb698d98bc0ad49a99215100b9d7
Hassan <hassangulzar121@gmail.com>
2023-02-12 15:25:13 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+// !Program No 1
+let namee = "Umair Azmat";
+let admin = namee;
+// alert(admin);
+
+// !Program No 2
+let earth;
+
+// !Program No 3
+let currentUser;
+
+// !Program No 4
+let favDrink = "Sting";
+
+// !Program No 5
+let favNumber = "1";
+
+// !Program No 6
+function myFavs() {
+  console.log("My name is Hassan MERN stack developer!");
+}
+myFavs();
+
+// !Program No 7
+let person = {
+  namee: "Umair Azmat",
+  age: 20,
+};
+
+// !Program No 8
+let a = 10;
+let b = 11;
+let aSumb = a + b;
+console.log(aSumb);
+
+// !Program No 9
+
+let divide = 10 / 2;
+console.log("result of 10 divide by 2 is: " + divide);
+
+// !Program No 10
+
+let add = 10 + 10;
+let divide1 = add / 5;
+console.log(divide1);
+
+// !Program No 11
+
+let f = 50;
+let c = ((f - 32) * 5) / 9;
+console.log("divide two number:  " + c);
+
+// !Program No 12 adding
+
+let sum1 = prompt("type a first number to add");
+let number1 = parseInt(sum1);
+let sum2 = prompt("type a second number to add in 1st value");
+let number2 = parseInt(sum2);
+console.log(`${number1} + ${number2} = ${number1 + number2}`);
+
+// !Program No 13 difference
+let diff1 = prompt("type a  first number for diff");
+let numDiff1 = parseInt(diff1);
+let diff2 = prompt("type a second number from value 1 diff");
+let numDiff2 = parseInt(diff2);
+console.log(`${numDiff1} - ${numDiff2} = ${numDiff1 - numDiff2}`);
+
+// !Program No 14 product
+let product1 = prompt("type a first number for product");
+let numProduct1 = parseInt(product1);
+let product2 = prompt("type a second number from value 1 product");
+let numProduct2 = parseInt(product2);
+console.log(`${numProduct1} * ${numProduct2} = ${numProduct1 * numProduct2}`);
+
+// !Program No 15 quotient
+let quotient1 = prompt("type first  number");
+let numQuo1 = parseInt(quotient1);
+let quotient2 = prompt("type a number to get a quotient from value 1");
+let numQuo2 = parseInt(quotient2);
+console.log(`${numQuo1} / ${numQuo2} = ${numQuo1 / numQuo2}`);
+
+//! Program No 16 remainder
+let remainder1 = prompt("type a first number");
+let numRem1 = parseInt(remainder1);
+let remainder2 = prompt("type a second number");
+let numRem2 = parseInt(remainder2);
+console.log(`${numRem1} % ${numRem2} = ${numRem1 % numRem2}`);
